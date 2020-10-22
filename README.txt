a95278fd1b2a626db305e91edc12279c5727d34f
vshah10 <vrushabhshah04@gmail.com>
2020-10-22 22:01:21 +0530

    first commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+Download all the files in a folder. Open terminal in the same folder.
+
+Type the command 'python capture.py' in terminal.
---
+++ a.c
@@ -0,0 +1,10 @@
+//This is The Coding Area
+#include<stdio.h>
+#include<conio.h>
+#include<math.io>
+int main()
+{
+  int N;
+  scanf("%d",&N);
+  
+}
